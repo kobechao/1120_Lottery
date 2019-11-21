353415fcc40a875c8cb94f71b56795bd200a3bfa
Chang Han Chih <changhanchih@hanchih.local>
2019-11-21 10:25:41 -0800

    modify

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,12 +1,12 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-import "./ERC20.sol";
+// import "./ERC20.sol";
 
 // import "https://github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol";
 
 contract Lottery {
 
-    uint public constant ticketPrice = 0.01 ether;
+    // uint public constant ticketPrice = 0.01 ether;
 
     // owner of the contract
     address payable public owner;
@@ -19,57 +19,61 @@ contract Lottery {
 
     // record of players who buy ticket
     // players show as address, need mapping to record "which address already buy ticket"
-    mapping (address=>bool) playerBook;
+    // mapping (address=>bool) playerBook;
 
     // 紀錄每個開獎號碼所有投入的玩家列表
-    mapping (uint=>address[]) playerData;
+    mapping (uint=>address payable[]) playerData;
 
     // 紀錄每個開獎號碼所有投入的金額
     mapping (uint=>uint) indexTotalValue;
+    
+    // 玩家投入金額
+    mapping (address=>uint) balances;
 
     // record all the addresses
     address payable[] playersAddress;
 
-    uint[] public RNDArray;
-    mapping (uint=>bool) RNDTable;
+    // uint[] public RNDArray;
+    // mapping (uint=>bool) RNDTable;
 
     // event: the only way to communicate with outside
     // indexed: speed up the search rate
-    event BuyTicket(address indexed _buyer, uint _spent, uint _buyTime);
+    event Bet(address indexed _buyer, uint _spent, uint _buyTime);
     event Winner(address indexed _winner, uint _amount, string msg);
-
+    
     // start contract
     constructor() public {
         owner = msg.sender;
         startGame();
-        generateRNDArr(3, 10);
+        // generateRNDArr(1, 10);
     }
 
-    // 遞迴產生給定長度的隨機數陣列與Mapping
-    function generateRNDArr(uint length, uint _nonce) internal {
-        if(RNDArray.length>=length)
-            return;
+    // // 遞迴產生給定長度的隨機數陣列與Mapping
+    // function generateRNDArr(uint length, uint _nonce) internal {
+    //     if(RNDArray.length>=length)
+    //         return;
             
-        uint rnd = _generateRND(_nonce++);
-        if(RNDTable[rnd] == false) {
-            RNDTable[rnd] = true;
-            RNDArray.push(rnd);
-        }
-        generateRNDArr(length, _nonce);
-    }
+    //     uint rnd = _generateRND(_nonce++);
+    //     if(RNDTable[rnd] == false) {
+    //         RNDTable[rnd] = true;
+    //         RNDArray.push(rnd);
+    //     }
+    //     generateRNDArr(length, _nonce);
+    // }
 
-    // 產生隨機數
-    function _generateRND(uint _nonce) internal view returns(uint8) {
-        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%_nonce);
-    }
+    // // 產生隨機數
+    // function _generateRND(uint _nonce) internal view returns(uint8) {
+    //     return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%_nonce);
+    // }
 
-    // 取得隨機數陣列
-    function getRNDArr() public view returns(uint[] memory, uint) {
-        return (RNDArray, RNDArray.length);
-    }
+    // // 取得隨機數陣列
+    // function getRNDArr() public view returns(uint[] memory, uint) {
+    //     return (RNDArray, RNDArray.length);
+    // }
 
     // 定義一些高權限事項的修改器
     // modifier: pre-process, check, verify
+    
     modifier onlyOwnerCanDoThis() {
         require(msg.sender==owner, "onlyOwnerCanDoThis");
         _;
@@ -91,7 +95,7 @@ contract Lottery {
     function startGame() internal {
         _isGameStarted = true;
         _startTime = now;
-        _endTime = _startTime + 2 minutes;
+        _endTime = _startTime + 1 minutes;
     }
 
     // exceed end time
@@ -102,33 +106,47 @@ contract Lottery {
     }
 
     function bet(uint _index) public payable {
-        // To-Do
-        // ...
-
+        
+        require(_index>=0 && _index<=9);
+        require(_isGameStarted == true);
+        require(_isGameEnded == false);
+        
+        uint value = msg.value;
+        indexTotalValue[_index] += value;
+        playerData[_index].push(msg.sender);
+        balances[msg.sender] = value;
+        emit Bet(msg.sender, msg.value, now);
+        
         if(now >= _endTime)
             endGame();
     }
-
+    
+    function getNum(uint _number) public view returns (uint, address payable[]  memory){
+        address payable[] memory _addrs = playerData[_number];
+        return (indexTotalValue[_number], _addrs);
+    }
+    
     function selectWinIndex() public view returns(uint) {
-        return uint(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%RNDArray.length);
+        return uint(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%10);
     }
 
     function sendProfitToWinners() internal {
         // address payable winner = selectWinner();
         uint winIndex = selectWinIndex();
         
-        // To-Do
-        // ...
-
-
-        // amount in contract address
-        // uint profit = address(this).balance;
-        // winner.transfer(profit);
+        uint amount = 0;
+        for(uint i=0; i<=9; i++)
+            amount += indexTotalValue[i];
         
-        // emit Winner(winner, profit, "Winner Found!");
-        
-        // prevent reentrancy attack
-        // profit = 0;
+        address payable[] memory winners = playerData[winIndex];
+        for(uint j=0; j<winners.length; j++){
+            address payable win = winners[j];
+            uint profit = amount*(balances[win]/indexTotalValue[winIndex]);
+            win.transfer(profit);
+            emit Winner(win, profit,"Winner Found!");
+            
+            //prevent reentrancy attack
+            profit = 0;            
+        }
     }
-    
 }
